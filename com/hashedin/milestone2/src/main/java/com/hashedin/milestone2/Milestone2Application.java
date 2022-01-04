96f654ac81f1584d25a458ff3f613970a6ba637a
atissrivastava <atissrivastava@deloitte.com>
2022-01-04 18:02:51 +0530

    Modified pom.xml

--- com/hashedin/milestone2/src/main/java/com/hashedin/milestone2/Milestone2Application.java
+++ com/hashedin/milestone2/src/main/java/com/hashedin/milestone2/Milestone2Application.java
@@ -2,11 +2,13 @@ package com.hashedin.milestone2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
-@SpringBootApplication
-@EnableWebSecurity
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Milestone2Application {
 
     public static void main(String[] args) {
